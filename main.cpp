b11b2722fb122c2d722fe004d7c2ee7646a2f7db
kevinthmm <kevinthmm@gmail.com>
2022-03-14 13:27:30 +0800

    test commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ICT1009 CPP Proj
+//
+//  Created by Kevin Thom on 14/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
